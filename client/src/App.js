5466f6e289c27f6a561ec46488de683b0b65c731
Brad <bradleofabian@gmail.com>
2018-10-07 22:37:21 -0400

    added faq.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Discover from "./pages/Discover";
+import About from "./pages/About";
+import FAQ from "./pages/faq";
+import Login from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" componnent={About} />
+        <Route exact path="/about" componnent={About} />
+        <Route exact path="/faq" componnent={FAQ} />
+        <Route exact path="/login" componnent={Login} />
+      </Wrapper>
+      <Footer />
+    </div>
+  </Router>
+);
 
 export default App;
