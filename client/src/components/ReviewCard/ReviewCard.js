adc449aee172b1efe2bc5ec3e406a4ec4e51900a
Brad <bradleofabian@gmail.com>
2018-10-11 18:27:59 -0400

    added stock photos

---
+++ client/src/components/ReviewCard/ReviewCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ReviewCard.css";
+
+const ReviewCard = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+    <div className="content">
+      <ul>
+        <li>
+          <strong>Name:</strong> {props.name}
+        </li>
+        <li>
+          <strong>Occupation:</strong> {props.occupation}
+        </li>
+        <li>
+          <strong>Review:</strong> {props.review}
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default ReviewCard;
--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,79 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import ReviewCard from "../components/ReviewCard";
 import Container from "../components/Container";
+import Wrapper from "../components/Wrapper";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import reviews from "../reviews.json";
 
-const About = () => (
-  <div>
-    <Container style={{ marginTop: 30 }}>
-      <Row>
-        <Col size="md-12">
-          <h1>Skill Assesement & Conducive Training Options for Teams</h1>
-        </Col>
-        <Col size="md-12">
-          <h3>Build successful, goal oriented teams by using our app to</h3>
-        </Col>
-        <Col size="md-12">
-          <h3>
-            Identify your employees potentials , quintify a great team
-            enviroment and
-          </h3>
-        </Col>
-        <Col size="md-12">
-          <h3>
-            Reccomend training for any discovered inadequacies within an
-            employees skills assesement.
-          </h3>
-        </Col>
-      </Row>
+class About extends Component {
+  state = {
+    reviews
+  };
 
-      <Row>
-        <Col size="md-4" />
-        <div className="card mb-4">
-          <img
-            className="card-img-top"
-            src="https://picsum.photos/200/300/?random"
-            alt="Happy Customer"
-          />
-          <div className="card-body">
-            <p className="card-text">
-              Robo Survey has helped our company target weaker employees and
-              train them to be more productive! Our bottom line has doubled!
-            </p>
-          </div>
-        </div>
-        <Col size="md-4" />
-        <div className="card mb-4">
-          <img
-            className="card-img-top"
-            src="https://picsum.photos/200/300/?random"
-            alt="Happy Customer"
-          />
-          <div className="card-body">
-            <p className="card-text">
-              My managers now have the tools to make their teams way more
-              effecient and productive! Thank You!
-            </p>
-          </div>
-        </div>
-
-        <Col size="md-4" />
-        <div className="card mb-4">
-          <img
-            className="card-img-top"
-            src="https://picsum.photos/200/300/?random"
-            alt="Happy Customer"
-          />
-          <div className="card-body">
-            <p className="card-text">
-              I did not like giving annual reviews but now I am armed with
-              methods on how to better train my staff.
-            </p>
-          </div>
-        </div>
-      </Row>
-    </Container>
-  </div>
-);
+  render() {
+    return (
+      <Wrapper>
+        <Container style={{ marginTop: 10 }}>
+          <Row>
+            <Col size="md-12">
+              <h1>Skill Assesement & Conducive Training Options for Teams</h1>
+            </Col>
+            <Col size="md-12">
+              <h3>Build successful, goal oriented teams by using our app to</h3>
+            </Col>
+            <Col size="md-12">
+              <h3>
+                Identify your employees potentials , quintify a great team
+                enviroment and
+              </h3>
+            </Col>
+            <Col size="md-12">
+              <h3>
+                Reccomend training for any discovered inadequacies within an
+                employees skills assesement.
+              </h3>
+            </Col>
+          </Row>
+          {this.state.reviews.map(review => (
+            <ReviewCard
+              id={review.id}
+              key={review.id}
+              name={review.name}
+              image={review.image}
+              occupation={review.occupation}
+              review={review.review}
+            />
+          ))}
+        </Container>
+      </Wrapper>
+    );
+  }
+}
 
 export default About;
