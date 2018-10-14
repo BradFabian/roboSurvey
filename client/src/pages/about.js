36cc625821489ae60386d5beb46e7107f36a8d1a
Brad <bradleofabian@gmail.com>
2018-10-14 11:50:18 -0400

    fixed about page

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import ReviewCard from "../components/ReviewCard";
-import Container from "../components/Container";
-import Wrapper from "../components/Wrapper";
-import Row from "../components/Row";
-import Col from "../components/Col";
-import reviews from "../reviews.json";
 
+import reviews from "../reviews.json";
+import { Container, Row, Col } from "mdbreact";
 class About extends Component {
   state = {
     reviews
@@ -13,28 +10,28 @@ class About extends Component {
 
   render() {
     return (
-      <Wrapper>
-        <Container style={{ marginTop: 10 }}>
-          <Row>
-            <Col size="md-12">
-              <h1>Skill Assesement & Conducive Training Options for Teams</h1>
-            </Col>
-            <Col size="md-12">
-              <h3>Build successful, goal oriented teams by using our app to</h3>
-            </Col>
-            <Col size="md-12">
-              <h3>
-                Identify your employees potentials , quintify a great team
-                enviroment and
-              </h3>
-            </Col>
-            <Col size="md-12">
-              <h3>
-                Reccomend training for any discovered inadequacies within an
-                employees skills assesement.
-              </h3>
-            </Col>
-          </Row>
+      <Container style={{ marginTop: 10 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Skill Assesement & Conducive Training Options for Teams</h1>
+          </Col>
+          <Col size="md-12">
+            <h3>Build successful, goal oriented teams by using our app to</h3>
+          </Col>
+          <Col size="md-12">
+            <h3>
+              Identify your employees potentials , quintify a great team
+              enviroment and
+            </h3>
+          </Col>
+          <Col size="md-12">
+            <h3>
+              Reccomend training for any discovered inadequacies within an
+              employees skills assesement.
+            </h3>
+          </Col>
+        </Row>
+        <Row>
           {this.state.reviews.map(review => (
             <ReviewCard
               id={review.id}
@@ -46,8 +43,8 @@ class About extends Component {
               review={review.review}
             />
           ))}
-        </Container>
-      </Wrapper>
+        </Row>
+      </Container>
     );
   }
 }
