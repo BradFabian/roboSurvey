4feaaa816e22414a92ab92533bf6e5157f39f55e
Brad <bradleofabian@gmail.com>
2018-10-14 11:52:00 -0400

    fixed login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { Container, Row, Col, Card, CardBody, Input, Button } from "mdbreact";
-
+import Login from "../components/Login";
 import image from "../assets/img/bg7.jpg";
 
 class LoginPage extends React.Component {
   render() {
     return (
       <div
+        className="container-fluid"
         style={{
           backgroundImage: "url(" + image + ")",
           backgroundSize: "cover",
-          backgroundPosition: "top center"
+          backgroundPosition: "top center",
+          paddingBottom: "30%",
+          paddingTop: "10%"
         }}
       >
-        <Login />
+        <div className="row">
+          <Login />
+        </div>
       </div>
     );
   }
