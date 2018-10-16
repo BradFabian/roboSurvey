a58bc108cd937240708e65e9b371d51e33c4934a
Brad <bradleofabian@gmail.com>
2018-10-16 11:30:52 -0400

    added axios api search

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/about";
 import FAQ from "./pages/faq";
 import Login from "./pages/login";
-import Manager from "./pages/manager"
+import Manager from "./pages/manager";
+import User from "./pages/user";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 
@@ -12,16 +13,15 @@ import "./App.css";
 const App = () => (
   <Router>
     <div>
-
       <Navbar />
 
       <Route exact path="/" component={About} />
       <Route exact path="/about" component={About} />
       <Route exact path="/faq" component={FAQ} />
       <Route exact path="/login" component={Login} />
-
+      <Route exact path="/user" component={User} />
+      <Route exact path="/manager" component={Manager} />
       <Footer />
-
     </div>
   </Router>
 );
---
+++ client/src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const APIURL = "/api/evaluations";
+const queryScores = "";
+export default {
+  //Gets all scores
+  getScores: function() {
+    return axios.get(APIURL + Scores);
+  }
+};
