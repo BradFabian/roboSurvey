eb9f40f5e593a2e01579d45b783b531305cf25f0
Joed Machado <joedmg58@gmail.com>
2018-10-23 23:17:45 -0400

    working...

--- client/src/components/DisplaySurvey/MultiChoiseAnswer.js
+++ client/src/components/DisplaySurvey/MultiChoiseAnswer.js
@@ -2,6 +2,49 @@ import React from 'react';
 
 class MultiChoiseAnswer extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: null
+        }
+
+        this.valueChange = this.valueChange.bind(this);
+    }
+
+    valueChange(event) {
+        this.setState({value: event.target.value});
+        this.props.onChange(event.target.value);
+
+        console.log(event.target.name + " - " + event.target.value);
+    }
+
+
+    render() {
+
+        return(
+            <div>
+                {this.props.answer}
+                <input 
+                    type = "radio" 
+                    name = {this.props.radioName} 
+                    value = {this.props.value}
+                    onChange = {this.valueChange}
+                /> 
+            </div>
+        )
+    }
+    
+}
+
+export default MultiChoiseAnswer;
+
+
+
+/* import React from 'react';
+
+class MultiChoiseAnswer extends React.Component {
+
     render() {
 
         return(
@@ -13,4 +56,4 @@ class MultiChoiseAnswer extends React.Component {
     
 }
 
-export default MultiChoiseAnswer;
+export default MultiChoiseAnswer; */
--- client/src/components/DisplaySurvey/Question.js
+++ client/src/components/DisplaySurvey/Question.js
@@ -3,6 +3,52 @@ import MultiChoiseAnswer from './MultiChoiseAnswer';
 
 class Question extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selected: null
+        }
+
+        this.changeValue = this.changeValue.bind(this);
+    }
+
+    changeValue(answer) {
+        this.setState({selected: answer});
+        this.props.onAnswer( this.props.questionNo, answer);
+
+        console.log("The selected answer for question "+this.props.questionNo+" is " + answer);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>{this.props.question}</h3>
+                {
+                    this.props.answers.map(
+                        (answer, i) => <MultiChoiseAnswer key = {i}
+                                                          answer = {answer}
+                                                          radioName = {"question"+this.props.questionNo}
+                                                          value = {i}
+                                                          onChange = {this.changeValue}
+                        />
+                    )
+                }
+            </div>
+        )
+    }
+
+}
+
+export default Question;
+
+
+
+/* import React from 'react';
+import MultiChoiseAnswer from './MultiChoiseAnswer';
+
+class Question extends React.Component {
+
     render() {
         return (
             <div>
@@ -25,4 +71,4 @@ class Question extends React.Component {
 
 }
 
-export default Question;
+export default Question; */
