8541b27bec12159b0f38263dc91e680b8d7f3e2a
oscar <oscar@o2webdev.com>
2018-10-24 18:46:31 -0400

    Apply joed fixed

--- client/src/components/DisplaySurvey/Question.js
+++ client/src/components/DisplaySurvey/Question.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import MultiChoiseAnswer from './MultiChoiseAnswer';
-
 class Question extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: null
+        }
+        this.changeValue = this.changeValue.bind(this);
+    }
+    changeValue(answer) {
+        this.setState({ selected: answer });
+        this.props.onAnswer(this.props.questionNo, answer);
+        console.log("The selected answer for question " + this.props.questionNo + " is " + answer);
+    }
     render() {
         return (
             <div>
-                <h5>
-                <strong className="blue-grey-text">{this.props.question}</strong>
-                </h5>
+                <h3>{this.props.question}</h3>
                 {
                     this.props.answers.map(
-                        (answer, i) => <MultiChoiseAnswer key = {i}
-                                                          radioName = {"question"+this.props.questionNo}
-                                                          answer = {answer}
-                                                          value = {i}
+                        (answer, i) => <MultiChoiseAnswer key={i}
+                            answer={answer}
+                            radioName={"question" + this.props.questionNo}
+                            value={i}
+                            onChange={this.changeValue}
                         />
                     )
                 }
-                <br></br>
             </div>
         )
     }
-
 }
-
 export default Question;
+/* import React from 'react';
+import MultiChoiseAnswer from './MultiChoiseAnswer';
+class Question extends React.Component {
+    render() {
+@@ -25,4 +71,4 @@ class Question extends React.Component {
+}
+export default Question;
+export default Question; */
