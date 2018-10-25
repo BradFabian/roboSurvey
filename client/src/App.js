330827ffdc4c0203b5467b54acdc13178419b826
oscar <oscar@o2webdev.com>
2018-10-24 20:08:09 -0400

    Apply fixed

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,33 @@ import FAQ from "./pages/faq";
 import Login from "./pages/login";
 import Manager from "./pages/manager";
 import User from "./pages/user";
-import DisplaySurvey from "./pages/survey";
+
+import DisplaySurvey from "./pages/survey"; //added
 
 import "./App.css";
 
-const App = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={About} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/faq" component={FAQ} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/user/:id" component={User} />
-      <Route exact path="/manager" component={Manager} />
-      <Route exact path="/survey/:id" component={DisplaySurvey} />
-    </div>
-  </Router>
-);
+const App = () => {
+
+
+  return (
+
+
+    <Router>
+      <div>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/faq" component={FAQ} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/user/:id" component={User} />
+        <Route exact path="/manager" component={Manager} />
+
+        {/** Added */}
+        <Route exact path="/survey/:id" component={DisplaySurvey} />
+
+      </div>
+    </Router>
+  );
+
+}
 
 export default App;
